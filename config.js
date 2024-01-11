4065ca5d4482bb4079f13c338cd2356b386e4e97
Mark Downey <mark@3dverse.com>
2024-01-11 11:35:33 +0000

    add config for demo

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const publicToken = %YOUR_PUBLIC_TOKEN%;
-export const mainSceneUUID = %YOUR_MAIN_SCENE_UUID%;
+export const publicToken = 'public_VTjawDe35xT5eDTX';
+export const mainSceneUUID = '953cb5d6-dc03-42e9-9419-e2aef7188079';
